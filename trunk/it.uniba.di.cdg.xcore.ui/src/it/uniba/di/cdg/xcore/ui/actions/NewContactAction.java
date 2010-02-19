8ad9d38ff07878804409be83332dd3ff7a89ae19
pol.alessano <pol.alessano@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-02-19 10:25:08 +0000

    added roster management functions

---
+++ trunk/it.uniba.di.cdg.xcore.ui/src/it/uniba/di/cdg/xcore/ui/actions/NewContactAction.java
@@ -0,0 +1,42 @@
+package it.uniba.di.cdg.xcore.ui.actions;
+
+import it.uniba.di.cdg.xcore.network.NetworkPlugin;
+import it.uniba.di.cdg.xcore.network.model.IBuddyRoster;
+import it.uniba.di.cdg.xcore.ui.UiPlugin;
+import it.uniba.di.cdg.xcore.ui.dialogs.ChangePasswordDialog;
+import it.uniba.di.cdg.xcore.ui.dialogs.NewContactDialog;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class NewContactAction extends Action implements ActionFactory.IWorkbenchAction {
+
+	public static final String ID = UiPlugin.ID + ".actions.ChangePasswordAction";
+	
+	public NewContactAction() {
+		super();
+    }
+	
+	public void run(IBuddyRoster roster) {
+	    //setEnabled( false ); // Will be re-enabled in done()
+	    Display display = Display.getDefault();
+		Shell shell = new Shell(display);
+		NewContactDialog contact = new NewContactDialog(shell, SWT.NULL);
+		if (NetworkPlugin.getDefault().getRegistry().getDefaultBackend().isConnected() == false){
+			UiPlugin.getUIHelper().showMessage("You must to be connected to the server in order to do this!");
+		}else {
+		    contact.open(roster);
+		}
+	    }
+	public void Enable (boolean valore) {
+		setEnabled(valore);
+	}
+	
+	@Override
+	public void dispose() {
+	}
+}
