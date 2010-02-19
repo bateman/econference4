b7d8d928ac53002dd4be0a24ce8c6bbd3eea0541
pol.alessano <pol.alessano@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-02-19 10:26:14 +0000

    added roster management functions

---
+++ it.uniba.di.cdg.xcore.one2one/src/it/uniba/di/cdg/xcore/one2one/ui/actions/popup/RenameBuddyAction.java
@@ -0,0 +1,56 @@
+/**
+ * This file is part of the eConference project and it is distributed under the 
+ * terms of the MIT Open Source license.
+ * 
+ * The MIT License
+ * Copyright (c) 2005 Collaborative Development Group - Dipartimento di Informatica, 
+ *                    University of Bari, http://cdg.di.uniba.it
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package it.uniba.di.cdg.xcore.one2one.ui.actions.popup;
+
+import it.uniba.di.cdg.xcore.one2one.ChatPlugin;
+import it.uniba.di.cdg.xcore.network.model.IBuddy;
+import it.uniba.di.cdg.xcore.ui.actions.AbstractBuddyActionDelegate;
+import it.uniba.di.cdg.xcore.ui.dialogs.RenameContactDialog;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Context menu action for opening an chat with another buddy.
+ */
+public class RenameBuddyAction extends AbstractBuddyActionDelegate {
+    /**
+     * Unique id of this action.
+     */
+    public static final String ID = ChatPlugin.ID + ".ui.actions.popup.renameBuddyAction";
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run( IAction action ) { 
+        final IBuddy buddy = getSelected();
+        Display display = Display.getDefault();
+        Shell shell = new Shell(display);
+        RenameContactDialog rename = new RenameContactDialog(shell, SWT.NULL);
+        rename.open( buddy );        
+    }
+}
