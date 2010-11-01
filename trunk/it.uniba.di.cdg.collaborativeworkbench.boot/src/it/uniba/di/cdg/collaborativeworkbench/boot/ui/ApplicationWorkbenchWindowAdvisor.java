273017c1e713e0431ad8bf0d2d3a15d29abc8823
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-11-01 11:32:28 +0000

    Fixed issue 46

--- trunk/it.uniba.di.cdg.collaborativeworkbench.boot/src/it/uniba/di/cdg/collaborativeworkbench/boot/ui/ApplicationWorkbenchWindowAdvisor.java
+++ trunk/it.uniba.di.cdg.collaborativeworkbench.boot/src/it/uniba/di/cdg/collaborativeworkbench/boot/ui/ApplicationWorkbenchWindowAdvisor.java
@@ -26,7 +26,6 @@ package it.uniba.di.cdg.collaborativeworkbench.boot.ui;
 
 import it.uniba.di.cdg.collaborativeworkbench.ui.BootPlugin;
 
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ShellAdapter;
 import org.eclipse.swt.events.ShellEvent;
@@ -36,234 +35,293 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tray;
 import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.handlers.IHandlerService;
 
 /**
- * Provides configuration for the workbench window and handles the workbench's life cycle.
+ * Provides configuration for the workbench window and handles the workbench's
+ * life cycle.
  */
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    //private static final String APP_TITLE = "Collaborative workbench";
-    private static final String APP_TITLE = "eConference";
-    
-    private TrayItem trayItem;
-    
-    private Image trayImage;
-    
-    private ApplicationActionBarAdvisor actionBarAdvisor;
+	// private static final String APP_TITLE = "Collaborative workbench";
+	private static final String APP_TITLE = "eConference";
+
+	private TrayItem trayItem;
+
+	private Image trayImage;
+
+	private ApplicationActionBarAdvisor actionBarAdvisor;
 
 	private boolean trayIconImage;
 
 	private Image trayNoImage;
-    
-    public ApplicationWorkbenchWindowAdvisor( IWorkbenchWindowConfigurer configurer ) {
-        super( configurer );
-    }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#createActionBarAdvisor(org.eclipse.ui.application.IActionBarConfigurer)
-     */
-    @Override
-    public ActionBarAdvisor createActionBarAdvisor( IActionBarConfigurer configurer ) {
-        actionBarAdvisor = new ApplicationActionBarAdvisor( configurer );
-        return actionBarAdvisor;
-    }
-    public boolean preWindowShellClose() {
-    	final IWorkbenchWindow window = getWindowConfigurer().getWindow();
-    	window.getShell().setMinimized(true);
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.application.WorkbenchWindowAdvisor#createActionBarAdvisor
+	 * (org.eclipse.ui.application.IActionBarConfigurer)
+	 */
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		actionBarAdvisor = new ApplicationActionBarAdvisor(configurer);
+		return actionBarAdvisor;
+	}
+
+	public boolean preWindowShellClose() {
+		final IWorkbenchWindow window = getWindowConfigurer().getWindow();
+		window.getShell().setMinimized(true);
 		return false;
-    	
-    }
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#preWindowOpen()
-     */
-    @Override
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        
-        configurer.setInitialSize( new Point( 320, 520 ) );
-        configurer.setShowCoolBar( true );
-        configurer.setShowStatusLine( true );
-        configurer.setShowProgressIndicator( false );
-        configurer.setShowPerspectiveBar( true );
-        configurer.setTitle( APP_TITLE );
-    }
-    
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#postWindowOpen()
-     */
-    @Override
-    public void postWindowOpen() {
-        super.postWindowOpen();
-        
-        //load chatPlugIn
 
-        //try {
-		//	Platform.getBundle("it.uniba.di.cdg.xcore.one2one").start();
-		//} catch (BundleException e) {
-		//	e.printStackTrace();
-		//}
+	}
 
-//        centerWorkbenchWindow();
-        
-        final IWorkbenchWindow window = getWindowConfigurer().getWindow();
-        getWindowConfigurer().getActionBarConfigurer();
-        trayItem = initTaskItem( window );
-        if (trayItem != null) {
-            hookPopupMenu( window );
-            hookMinimize( window );
-        }
-        //window.addPageListener(new pag)
-        
-        
-        final BackendEventListener daemon = new BackendEventListener();
-        daemon.start();
-        
-        new Thread(new Runnable() {
-            public void run() {
-               while (true) {
-                  try { Thread.sleep(1000); } catch (Exception e) { }
-                  Display.getDefault().asyncExec(new Runnable() {
-                     public void run() {
-                        //... do any work that updates the screen ...
-                    	 if (daemon.getMessageIncoming()) {
-                    		 //if (!window.getShell().isVisible()) {
-                    			if (!window.getShell().isFocusControl()) {
-                    				 if (trayIconImage) {
-                    					 setTrayIconNoImage();
-                    				 }
-                    				 else {
-                    					 setTrayIconImage();
-                    				 }
-                    			 
-                    		 } else {
-                    			 daemon.setNewMessageIncoming(false);
-                    			 setTrayIconImage();
-                    		 }
-                    	 }
-                     }
-                  });
-               }
-            }
-         }).start();
-        
-        
-        
-        window.getShell().addShellListener(new ShellAdapter(){
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#preWindowOpen()
+	 */
+	@Override
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
 
+		configurer.setInitialSize(new Point(320, 520));
+		configurer.setShowCoolBar(true);
+		configurer.setShowStatusLine(true);
+		configurer.setShowProgressIndicator(false);
+		configurer.setShowPerspectiveBar(true);
+		configurer.setTitle(APP_TITLE);
+	}
 
-        	public void shellActivated(ShellEvent e) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#postWindowOpen()
+	 */
+	@Override
+	public void postWindowOpen() {
+		super.postWindowOpen();
 
-        		daemon.setNewMessageIncoming(false);
-   			 	setTrayIconImage();
-        	}
-        	});
+		// load chatPlugIn
 
-     
-        
-    }
+		// try {
+		// Platform.getBundle("it.uniba.di.cdg.xcore.one2one").start();
+		// } catch (BundleException e) {
+		// e.printStackTrace();
+		// }
 
-    /**
-     * @param window
-     */
-    private void hookMinimize( final IWorkbenchWindow window ) {
-        window.getShell().addShellListener( new ShellAdapter() {
-            public void shellIconified( ShellEvent e ) {
-                //window.getShell().setVisible( false );
-            }
-        });
-        trayItem.addListener( SWT.DefaultSelection, new Listener() {
+		// centerWorkbenchWindow();
+
+		final IWorkbenchWindow window = getWindowConfigurer().getWindow();
+		getWindowConfigurer().getActionBarConfigurer();
+		trayItem = initTaskItem(window);
+		if (trayItem != null) {
+			hookPopupMenu(window);
+			hookMinimize(window);
+		}
+		// window.addPageListener(new pag)
+
+		final BackendEventListener daemon = new BackendEventListener();
+		daemon.start();
+
+		new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(1000);
+					} catch (Exception e) {
+					}
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
+							// ... do any work that updates the screen ...
+							if (daemon.getMessageIncoming()) {
+								// if (!window.getShell().isVisible()) {
+								if (!window.getShell().isFocusControl()) {
+									if (trayIconImage) {
+										setTrayIconNoImage();
+									} else {
+										setTrayIconImage();
+									}
+
+								} else {
+									daemon.setNewMessageIncoming(false);
+									setTrayIconImage();
+								}
+							}
+						}
+					});
+				}
+			}
+		}).start();
+
+		window.getShell().addShellListener(new ShellAdapter() {
+
+			public void shellActivated(ShellEvent e) {
+
+				daemon.setNewMessageIncoming(false);
+				setTrayIconImage();
+			}
+		});
+
+	}
+
+	/**
+	 * @param window
+	 */
+	private void hookMinimize(final IWorkbenchWindow window) {
+		window.getShell().addShellListener(new ShellAdapter() {
+			public void shellIconified(ShellEvent e) {
+				// window.getShell().setVisible( false );
+			}
+		});
+		trayItem.addListener(SWT.DefaultSelection, new Listener() {
 			@Override
 			public void handleEvent(Event event) {
-				// TODO Auto-generated method stub
 				Shell shell = window.getShell();
-               // if (!shell.isVisible()) {
-                    shell.setVisible( true );
-                    window.getShell().setMinimized( false );
-                    shell.setActive();
-               // }
+				// if (!shell.isVisible()) {
+				shell.setVisible(true);
+				window.getShell().setMinimized(false);
+				shell.setActive();
+				// }
 			}
-            
-        });
-    }
 
-    private void hookPopupMenu( final IWorkbenchWindow window ) {
-        trayItem.addListener( SWT.MenuDetect, new Listener() {
-            public void handleEvent( Event event ) {
-                MenuManager trayMenu = new MenuManager();
-                Menu menu = trayMenu.createContextMenu( window.getShell() );
-                actionBarAdvisor.fillTrayItem( trayMenu );
-                menu.setVisible( true );
-            }
-        });
-    }
+		});
+	}
 
-    private TrayItem initTaskItem( IWorkbenchWindow window ) {
-        final Tray tray = window.getShell().getDisplay().getSystemTray();
-        if (tray == null)
-            return null;
-        TrayItem trayItem = new TrayItem( tray, SWT.NONE );
-        trayImage = BootPlugin.getImageDescriptor( "icons/collab_tray.gif" ).createImage();
-        trayNoImage = BootPlugin.getImageDescriptor( "icons/collab_tray_NewMessage.gif" ).createImage();
-        
-        trayItem.setImage( trayImage );
-        trayIconImage=true;
-        //setTrayIconImage();
-        trayItem.setToolTipText( "eConference" );
-        
-        return trayItem;
-    }
-    private void setTrayIconImage() {
-    	//trayImage = BootPlugin.getImageDescriptor( "icons/collab_tray.gif" ).createImage();
-        trayItem.setImage( trayImage );
-        trayIconImage=true;
-    }
-    public void setTrayIconNoImage() {
-    	
-        trayItem.setImage(trayNoImage);
-        trayIconImage=false;
-    }
-//    private void centerWorkbenchWindow() {
-//        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-//        
-//        Display display = configurer.getWindow().getWorkbench().getDisplay();
-//        Shell shell = configurer.getWindow().getShell();
-//
-//        // Get the resolution
-//        Rectangle pDisplayBounds = display.getBounds();
-//
-//        int nMinWidth = 260;
-//        int nMinHeight = 520;
-        
-        // Set Width and Height(doesn't matter value)
-//        int nWidth = pDisplayBounds.width / 2;
-//        int nHeight = pDisplayBounds.height / 2;
+	private void hookPopupMenu(final IWorkbenchWindow window) {
+		// Creates a new menu item that terminates the program
+		// when selected
+		final Menu menu = new Menu(window.getShell(), SWT.POP_UP);
+		final MenuItem exit = new MenuItem(menu, SWT.NONE);
+		exit.setText("Exit");
+		exit.addListener(SWT.Selection, new Listener() {
+			private String COMMAND_ID = "it.uniba.di.cdg.xcore.econference.ui.commands.exit";
 
-        // This formulae calculate the shell's Left ant Top
-//        int nLeft = (pDisplayBounds.width - nMinWidth) / 2;
-//        int nTop = (pDisplayBounds.height - nMinHeight) / 2;
-//
-//        // Set shell bounds,
-//        shell.setBounds( nLeft, nTop, nMinWidth, nMinHeight );
-//    }
+			public void handleEvent(Event event) {
+				// Lets call our command
+				IHandlerService handlerService = (IHandlerService) window
+						.getService(IHandlerService.class);
+				try {
+					handlerService.executeCommand(COMMAND_ID, null);
+				} catch (Exception ex) {
+					throw new RuntimeException(COMMAND_ID);
+				}
+			}
+		});
 
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#dispose()
-     */
-    @Override
-    public void dispose() {
-        super.dispose();
-        
-        if (trayItem != null) {
-            trayImage.dispose();
-            trayItem.dispose();
-        }
-    }
+		// Creates a new menu item that shows the program about
+		// when selected
+		final MenuItem about = new MenuItem(menu, SWT.NONE);
+		about.setText("About");
+		about.addListener(SWT.Selection, new Listener() {
+
+			public void handleEvent(Event event) {
+				// Lets call our command
+				IHandlerService handlerService = (IHandlerService) window
+						.getService(IHandlerService.class);
+				try {
+					handlerService
+							.executeCommand(
+									IWorkbenchCommandConstants.HELP_ABOUT,
+									null);
+				} catch (Exception ex) {
+					throw new RuntimeException(
+							IWorkbenchCommandConstants.HELP_ABOUT);
+				}
+			}
+		});
+		
+		trayItem.addListener(SWT.MenuDetect, new Listener() {
+			public void handleEvent(Event event) {			
+				// We need to make the menu visible
+				menu.setVisible(true);
+			}
+		});
+	}
+
+	private TrayItem initTaskItem(IWorkbenchWindow window) {
+		final Tray tray = window.getShell().getDisplay().getSystemTray();
+		if (tray == null)
+			return null;
+		TrayItem trayItem = new TrayItem(tray, SWT.NONE);
+		trayImage = BootPlugin.getImageDescriptor("icons/collab_tray.gif")
+				.createImage();
+		trayNoImage = BootPlugin.getImageDescriptor(
+				"icons/collab_tray_NewMessage.gif").createImage();
+
+		trayItem.setImage(trayImage);
+		trayIconImage = true;
+		// setTrayIconImage();
+		trayItem.setToolTipText("eConference");
+
+		return trayItem;
+	}
+
+	private void setTrayIconImage() {
+		// trayImage = BootPlugin.getImageDescriptor( "icons/collab_tray.gif"
+		// ).createImage();
+		trayItem.setImage(trayImage);
+		trayIconImage = true;
+	}
+
+	public void setTrayIconNoImage() {
+
+		trayItem.setImage(trayNoImage);
+		trayIconImage = false;
+	}
+
+	// private void centerWorkbenchWindow() {
+	// IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+	//
+	// Display display = configurer.getWindow().getWorkbench().getDisplay();
+	// Shell shell = configurer.getWindow().getShell();
+	//
+	// // Get the resolution
+	// Rectangle pDisplayBounds = display.getBounds();
+	//
+	// int nMinWidth = 260;
+	// int nMinHeight = 520;
+
+	// Set Width and Height(doesn't matter value)
+	// int nWidth = pDisplayBounds.width / 2;
+	// int nHeight = pDisplayBounds.height / 2;
+
+	// This formulae calculate the shell's Left ant Top
+	// int nLeft = (pDisplayBounds.width - nMinWidth) / 2;
+	// int nTop = (pDisplayBounds.height - nMinHeight) / 2;
+	//
+	// // Set shell bounds,
+	// shell.setBounds( nLeft, nTop, nMinWidth, nMinHeight );
+	// }
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#dispose()
+	 */
+	@Override
+	public void dispose() {
+		super.dispose();
+
+		if (trayItem != null) {
+			trayImage.dispose();
+			trayItem.dispose();
+		}
+	}
 }
