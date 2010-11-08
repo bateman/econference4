b41952dddea4b57774e5782039ef21b03442fa42
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-11-08 19:07:33 +0000

    ==== Changelog ===== * tutorial and code updated accordingly

--- trunk/it.uniba.di.cdg.xcore.one2oneinfo/src/it/uniba/di/cdg/xcore/one2oneinfo/InfoDeamon.java
+++ trunk/it.uniba.di.cdg.xcore.one2oneinfo/src/it/uniba/di/cdg/xcore/one2oneinfo/InfoDeamon.java
@@ -7,41 +7,45 @@ import it.uniba.di.cdg.xcore.network.action.IChatServiceActions;
 import it.uniba.di.cdg.xcore.network.events.IBackendEvent;
 import it.uniba.di.cdg.xcore.network.events.IBackendEventListener;
 import it.uniba.di.cdg.xcore.network.events.chat.ChatExtensionProtocolEvent;
+import it.uniba.di.cdg.xcore.ui.UiPlugin;
 
 import java.util.HashMap;
 
-import javax.swing.JOptionPane;
-
 public class InfoDeamon implements IBackendEventListener {
-	
- public void start(){
-  for (IBackendDescriptor d : NetworkPlugin.getDefault()
-		  .getRegistry().getDescriptors())
-   NetworkPlugin.getDefault().getHelper().registerBackendListener(
-		   d.getId(), this);
- }
- @Override
- public void onBackendEvent(IBackendEvent event) {
-  if (event instanceof ChatExtensionProtocolEvent) {
-   IBackend b = NetworkPlugin.getDefault().getRegistry().getDefaultBackend();
-   IChatServiceActions chat = b.getChatServiceAction();
-   ChatExtensionProtocolEvent cepe = (ChatExtensionProtocolEvent)event;
-   if(cepe.getExtensionName().equals("GET_USER_INFO")){
-    HashMap<String, String> param = new HashMap<String, String>();
-	param.put("OS_NAME", System.getProperty("os.name"));
-	param.put("OS_VER", System.getProperty("os.version"));
-	chat.OpenChat(cepe.getFrom());
-	chat.SendExtensionProtocolMessage(cepe.getFrom(),
-			"USER_INFO", param);
-	chat.CloseChat(cepe.getFrom());
-   }
-   if(cepe.getExtensionName().equals("USER_INFO")){
-	String osName = (String) cepe.getExtensionParameter("OS_NAME");
-	String osVer = (String) cepe.getExtensionParameter("OS_VER");
-	JOptionPane.showMessageDialog(null, osName + " " + osVer);
-	chat.CloseChat(cepe.getFrom());
-	}
-   }
- }
-} 
 
+	public void start() {
+		for (IBackendDescriptor d : NetworkPlugin.getDefault().getRegistry()
+				.getDescriptors())
+			NetworkPlugin.getDefault().getHelper()
+					.registerBackendListener(d.getId(), this);
+	}
+
+	@Override
+	public void onBackendEvent(IBackendEvent event) {
+		if (event instanceof ChatExtensionProtocolEvent) {
+			IBackend b = NetworkPlugin.getDefault().getRegistry()
+					.getDefaultBackend();
+			IChatServiceActions chat = b.getChatServiceAction();
+			ChatExtensionProtocolEvent cepe = (ChatExtensionProtocolEvent) event;
+			if (cepe.getExtensionName().equals("GET_USER_INFO")) {
+				HashMap<String, String> param = new HashMap<String, String>();
+				param.put("OS_NAME", System.getProperty("os.name"));
+				param.put("OS_VER", System.getProperty("os.version"));
+				chat.OpenChat(cepe.getFrom());
+				chat.SendExtensionProtocolMessage(cepe.getFrom(), "USER_INFO",
+						param);
+				chat.CloseChat(cepe.getFrom());
+			}
+			if (cepe.getExtensionName().equals("USER_INFO")) {
+				String osName = (String) cepe.getExtensionParameter("OS_NAME");
+				String osVer = (String) cepe.getExtensionParameter("OS_VER");
+				showMessage("One2OneInfo plugin", "OS: " + osName + " ver: " + osVer);
+				chat.CloseChat(cepe.getFrom());
+			}
+		}
+	}
+
+	private void showMessage(String windowTitle, String message) {
+		UiPlugin.getUIHelper().showMessage(windowTitle, message);
+	}
+}
