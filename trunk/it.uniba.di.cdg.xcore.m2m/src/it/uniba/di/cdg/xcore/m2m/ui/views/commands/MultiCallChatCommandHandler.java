0fc29080eab2afd02cd2ddb13b0dfd3a0585d56d
giuseppesantoro87@gmail.com <giuseppesantoro87@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-11-13 19:34:21 +0000

    Fixed issue 38

--- trunk/it.uniba.di.cdg.xcore.m2m/src/it/uniba/di/cdg/xcore/m2m/ui/views/commands/MultiCallChatCommandHandler.java
+++ trunk/it.uniba.di.cdg.xcore.m2m/src/it/uniba/di/cdg/xcore/m2m/ui/views/commands/MultiCallChatCommandHandler.java
@@ -1,5 +1,6 @@
 package it.uniba.di.cdg.xcore.m2m.ui.views.commands;
 
+import it.uniba.di.cdg.xcore.m2m.IMultiChatManager;
 import it.uniba.di.cdg.xcore.network.IBackend;
 import it.uniba.di.cdg.xcore.network.NetworkPlugin;
 
@@ -9,8 +10,22 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 
 public class MultiCallChatCommandHandler extends AbstractHandler implements IHandler {  
+	
+	IMultiChatManager multiChatManager = null;
+	
+	public MultiCallChatCommandHandler(IMultiChatManager multiChatManager) {
+		super();
+		this.multiChatManager = multiChatManager;
+	}
+
+	public IMultiChatManager getMultiChatManager() {
+		return multiChatManager;
+	}
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		//execute the multi-chat call
      	IBackend b = NetworkPlugin.getDefault().getRegistry().getDefaultBackend();
      	if(b.getMultiCallAction().isCalling())
      		b.getMultiCallAction().finishCall();
