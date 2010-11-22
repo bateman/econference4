92880967b7a20c59403e4b16c4dda4b85c92ffab
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2010-11-22 23:39:39 +0000

    ==== Changelog ===== * initial commit to solve issue 51. added a thread.sleep(1000) to prevent synch issues

--- trunk/it.uniba.di.cdg.xcore.econference/src/it/uniba/di/cdg/xcore/econference/util/FolderMonitor.java
+++ trunk/it.uniba.di.cdg.xcore.econference/src/it/uniba/di/cdg/xcore/econference/util/FolderMonitor.java
@@ -1,3 +1,28 @@
+/**
+ * This file is part of the eConference project and it is distributed under the 
+
+ * terms of the MIT Open Source license.
+ * 
+ * The MIT License
+ * Copyright (c) 2010 Collaborative Development Group - Dipartimento di Informatica, 
+ *                    University of Bari, http://cdg.di.uniba.it
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package it.uniba.di.cdg.xcore.econference.util;
 
 import java.io.IOException;
@@ -28,8 +53,7 @@ public class FolderMonitor implements Runnable {
 		watchService = FileSystems.getDefault().newWatchService();
 		Path path = Paths.get(folder);
 		try {
-			path.register(watchService,
-					StandardWatchEventKind.ENTRY_CREATE,
+			path.register(watchService, StandardWatchEventKind.ENTRY_CREATE,
 					StandardWatchEventKind.ENTRY_MODIFY);
 		} catch (UnsupportedOperationException uox) {
 			System.err.println("File watching not supported!\nPath " + folder
@@ -42,48 +66,50 @@ public class FolderMonitor implements Runnable {
 
 	@Override
 	public void run() {
-		for(;;){
-		    // take() will block until a file has been created/deleted
-		    WatchKey signalledKey;
-		    try {
-		        signalledKey = watchService.take();
-		    } catch (InterruptedException ix){
-		        // we'll ignore being interrupted
-		        continue;
-		    } catch (ClosedWatchServiceException cwse){
-		        // other thread closed watch service
-		        System.out.println("watch service closed, terminating.");
-		        break;
-		    }
+		for (;;) {
+			// take() will block until a file has been created/deleted
+			WatchKey signalledKey;
+			try {
+				signalledKey = watchService.take();
+			} catch (InterruptedException ix) {
+				// we'll ignore being interrupted
+				continue;
+			} catch (ClosedWatchServiceException cwse) {
+				// other thread closed watch service
+				System.out.println("watch service closed, terminating.");
+				break;
+			}
 
-		    // get list of events from key
-		    List<WatchEvent<?>> list = signalledKey.pollEvents();
+			// get list of events from key
+			List<WatchEvent<?>> list = signalledKey.pollEvents();
 
-		    // VERY IMPORTANT! call reset() AFTER pollEvents() to allow the
-		    // key to be reported again by the watch service
-		    signalledKey.reset();
+			// VERY IMPORTANT! call reset() AFTER pollEvents() to allow the
+			// key to be reported again by the watch service
+			signalledKey.reset();
 
-		    // we'll simply print what has happened; real applications
-		    // will do something more sensible here
-		    for(WatchEvent<?> e : list){
-		        String message = "";
-		        if(e.kind() == StandardWatchEventKind.ENTRY_CREATE || e.kind() == StandardWatchEventKind.ENTRY_DELETE || e.kind() == StandardWatchEventKind.ENTRY_MODIFY){
-		            Path context = (Path)e.context();
-		            message = context.toString() + " change occurred.";
-		            System.out.println(message);	
-		            // hold on a sec before notifying
-		            try {
+			// we'll simply print what has happened; real applications
+			// will do something more sensible here
+			for (WatchEvent<?> e : list) {
+				String message = "";
+				if (e.kind() == StandardWatchEventKind.ENTRY_CREATE
+						|| e.kind() == StandardWatchEventKind.ENTRY_DELETE
+						|| e.kind() == StandardWatchEventKind.ENTRY_MODIFY) {
+					Path context = (Path) e.context();
+					message = context.toString() + " change occurred.";
+					System.out.println(message);
+					// hold on a sec before notifying
+					try {
 						Thread.sleep(1000);
-					} catch (InterruptedException e1) {						
+					} catch (InterruptedException e1) {
 					}
-			        listener.notifyFolderChange();
-		        } else if(e.kind() == StandardWatchEventKind.OVERFLOW){
-		            message = "OVERFLOW: more changes happened than we could retreive";
-		            System.out.println(message);
-		        }
-		    }
+					listener.notifyFolderChange();
+				} else if (e.kind() == StandardWatchEventKind.OVERFLOW) {
+					message = "OVERFLOW: more changes happened than we could retreive";
+					System.out.println(message);
+				}
+			}
 		}
-		
+
 	}
 
 }
