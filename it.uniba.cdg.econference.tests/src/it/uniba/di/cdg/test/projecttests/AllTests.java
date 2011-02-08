c84ecc537e82a80eefd45d82b8d93590d4f47f8d
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-02-08 15:47:36 +0000

    commit for issue 30

---
+++ it.uniba.cdg.econference.tests/src/it/uniba/di/cdg/test/projecttests/AllTests.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of the eConference project and it is distributed under the 
+ * terms of the MIT Open Source license.
+ * 
+ * The MIT License
+ * Copyright (c) 2011 Collaborative Development Group - Dipartimento di Informatica, 
+ *                    University of Bari, http://cdg.di.uniba.it
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package it.uniba.di.cdg.test.projecttests;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		it.uniba.di.cdg.collaborativeworkbench.boot.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.jabber.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.skype.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.xcore.econference.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.xcore.m2m.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.xcore.network.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.xcore.ui.test.allpackagetests.AllTests.class,
+		it.uniba.di.cdg.xcore.util.test.allpackagetests.AllTests.class })
+		
+public class AllTests {
+
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { AllTests.class });
+	}
+
+}
