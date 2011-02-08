d4d4ffd497f4f15efd1ccc63e1c01a829d5d48b5
cdesold@gmail.com <cdesold@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-02-08 12:30:59 +0000

    initial commit to fix issue 30

--- trunk/it.uniba.di.cdg.skype.test/src/it/uniba/di/cdg/skype/test/allpackagetests/AllTests.java
+++ trunk/it.uniba.di.cdg.skype.test/src/it/uniba/di/cdg/skype/test/allpackagetests/AllTests.java
@@ -22,53 +22,28 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package it.uniba.di.cdg.skype.test;
+package it.uniba.di.cdg.skype.test.allpackagetests;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
-import it.uniba.di.cdg.skype.SkypeBackend;
-import it.uniba.di.cdg.xcore.network.INetworkBackendHelper;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-import org.jmock.Mock;
-import org.jmock.cglib.MockObjectTestCase;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	it.uniba.di.cdg.skype.test.AllTests.class
+})
+public class AllTests {
 
-public class SkypeBackendTest extends MockObjectTestCase {
-
-	public void testGetHelper() {
-		SkypeBackend backend = new SkypeBackend();
-		Mock mock = mock(INetworkBackendHelper.class);
-		backend.setHelper((INetworkBackendHelper)
-				mock.proxy());
-		assertEquals(mock.proxy(), backend.getHelper());
-	}
-
-	public void testIsConnected() {
-		SkypeBackend backend = new SkypeBackend();
-	
-		Mock mock = mock(INetworkBackendHelper.class);
-		mock.stubs().method("notifyBackendEvent");
-		
-		backend.setHelper((INetworkBackendHelper) mock.proxy());
-		
-		try {
-			assertFalse(backend.isConnected());
-			backend.connect(null, null);
-			assertTrue(backend.isConnected());
-			backend.disconnect();
-			assertFalse(backend.isConnected());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void testGetBackendId() {
-		SkypeBackend backend = new SkypeBackend();
-		assertEquals("it.uniba.di.cdg.skype.skypeBackend",
-				backend.getBackendId());
-	}
-
-	public void testGetUserId() {
-		SkypeBackend backend = new SkypeBackend();
-		assertEquals(backend.getUserId(),
-				backend.getUserAccount().getId());
-		
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 07/02/11 12.23
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { AllTests.class });
 	}
 }
