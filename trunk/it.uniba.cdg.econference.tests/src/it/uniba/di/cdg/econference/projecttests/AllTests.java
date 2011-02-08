c10ce3d86bfe8ffa0ce61b16862f85ae339f5f0d
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-02-08 16:01:20 +0000

    renamed package

--- trunk/it.uniba.cdg.econference.tests/src/it/uniba/di/cdg/econference/projecttests/AllTests.java
+++ trunk/it.uniba.cdg.econference.tests/src/it/uniba/di/cdg/econference/projecttests/AllTests.java
@@ -22,7 +22,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package it.uniba.di.cdg.test.projecttests;
+package it.uniba.di.cdg.econference.projecttests;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
