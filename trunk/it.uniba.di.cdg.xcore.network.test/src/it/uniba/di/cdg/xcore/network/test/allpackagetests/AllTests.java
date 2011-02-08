62d6a461f93555e611357344efd9ade539c1c9c2
cdesold@gmail.com <cdesold@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-02-08 12:39:54 +0000

    initial commit to fix issue 30

---
+++ trunk/it.uniba.di.cdg.xcore.network.test/src/it/uniba/di/cdg/xcore/network/test/allpackagetests/AllTests.java
@@ -0,0 +1,62 @@
+/**
+ * This file is part of the eConference project and it is distributed under the 
+ * terms of the MIT Open Source license.
+ * 
+ * The MIT License
+ * Copyright (c) 2005 Collaborative Development Group - Dipartimento di Informatica, 
+ *                    University of Bari, http://cdg.di.uniba.it
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package it.uniba.di.cdg.xcore.network.test.allpackagetests;
+import it.uniba.di.cdg.xcore.network.JabberAccountTestCase;
+import it.uniba.di.cdg.xcore.network.internal.BackendRegistryTestCase;
+import it.uniba.di.cdg.xcore.network.internal.NetworkServiceRegistryTestCase;
+import it.uniba.di.cdg.xcore.network.model.tv.EntryTestCase;
+import it.uniba.di.cdg.xcore.network.model.tv.TalkModelTestCase;
+import it.uniba.di.cdg.xcore.network.services.CapabilitiesTestCase;
+import it.uniba.di.cdg.xcore.network.services.CapabilityTestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	it.uniba.di.cdg.xcore.network.AllTests.class,
+	it.uniba.di.cdg.xcore.network.internal.AllTests.class,
+	it.uniba.di.cdg.xcore.network.model.tv.AllTests.class,
+	it.uniba.di.cdg.xcore.network.services.AllTests.class,
+})
+public class AllTests {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 07/02/11 12.23
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { AllTests.class });
+	}
+}
