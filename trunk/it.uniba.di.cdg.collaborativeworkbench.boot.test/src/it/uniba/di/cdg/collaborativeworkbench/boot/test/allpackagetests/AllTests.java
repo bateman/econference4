48397605b80bf4911da3ff21edecd3a5ce093d25
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-03-29 15:27:35 +0000

    organized imports

--- trunk/it.uniba.di.cdg.collaborativeworkbench.boot.test/src/it/uniba/di/cdg/collaborativeworkbench/boot/test/allpackagetests/AllTests.java
+++ trunk/it.uniba.di.cdg.collaborativeworkbench.boot.test/src/it/uniba/di/cdg/collaborativeworkbench/boot/test/allpackagetests/AllTests.java
@@ -1,11 +1,5 @@
 package it.uniba.di.cdg.collaborativeworkbench.boot.test.allpackagetests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
