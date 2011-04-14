00c46ae628e1a2a3864d80efab594004c67b090f
pinto.tommaso22@gmail.com <pinto.tommaso22@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2011-04-14 11:52:54 +0000

    fixed issue 3

--- it.uniba.di.cdg.collaborativeworkbench.boot.test/src/it/uniba/di/cdg/collaborativeworkbench/boot/test/BackendEventListenerTest.java
+++ it.uniba.di.cdg.collaborativeworkbench.boot.test/src/it/uniba/di/cdg/collaborativeworkbench/boot/test/BackendEventListenerTest.java
@@ -24,74 +24,137 @@
  */
 package it.uniba.di.cdg.collaborativeworkbench.boot.test;
 
+import it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener;
+import it.uniba.di.cdg.xcore.network.events.chat.ChatMessageReceivedEvent;
+import it.uniba.di.cdg.xcore.network.events.multichat.MultiChatMessageEvent;
+
 import java.util.Collection;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
-
-import it.uniba.di.cdg.xcore.network.events.chat.ChatMessageReceivedEvent;
-import it.uniba.di.cdg.xcore.network.events.multichat.MultiChatMessageEvent;
-
-
-
 public class BackendEventListenerTest extends TestCase {
 
 	/**
-	 * Test method for {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#onBackendEvent(it.uniba.di.cdg.xcore.network.events.IBackendEvent)}.
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#onBackendEvent(it.uniba.di.cdg.xcore.network.events.IBackendEvent)}
+	 * .
 	 */
 	@Test
 	public void testOnBackendEvent() {
 		BackendEventListener daemon = new BackendEventListener();
-		assertFalse(daemon.getMessageIncoming());
-		daemon.onBackendEvent(new ChatMessageReceivedEvent(new TestBuddy(),"TestMessaggio","ID_TEST"));
-		assertTrue(daemon.getMessageIncoming());
-		
+		assertFalse(daemon.getIncomingMessage());
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(new TestBuddy(),
+				"TestMessaggio", "ID_TEST"));
+		assertTrue(daemon.getIncomingMessage());
+
 		daemon.setNewMessageIncoming(false);
-		assertFalse(daemon.getMessageIncoming());
-		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST","Test_MultiChatMessage", "From: Test_Multichat"));
-		assertTrue(daemon.getMessageIncoming());
-		
+		assertFalse(daemon.getIncomingMessage());
+		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST",
+				"Test_MultiChatMessage", "From: Test_Multichat"));
+		assertTrue(daemon.getIncomingMessage());
+
 	}
 
 	/**
-	 * Test method for {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#getMessageIncoming()}.
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#getMessageIncoming()}
+	 * .
 	 */
 	@Test
 	public void testGetMessageIncoming() {
 		BackendEventListener daemon = new BackendEventListener();
-		assertFalse(daemon.getMessageIncoming());
+		assertFalse(daemon.getIncomingMessage());
 		daemon.setNewMessageIncoming(true);
-		assertTrue(daemon.getMessageIncoming());
+		assertTrue(daemon.getIncomingMessage());
 		daemon.setNewMessageIncoming(false);
-		assertFalse(daemon.getMessageIncoming());
+		assertFalse(daemon.getIncomingMessage());
 	}
 
 	/**
-	 * Test method for {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#setNewMessageIncoming(boolean)}.
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#setNewMessageIncoming(boolean)}
+	 * .
 	 */
 	@Test
 	public void testSetNewMessageIncoming() {
 		BackendEventListener daemon = new BackendEventListener();
 		daemon.setNewMessageIncoming(true);
-		assertTrue(daemon.getMessageIncoming());
+		assertTrue(daemon.getIncomingMessage());
 		daemon.setNewMessageIncoming(false);
-		assertFalse(daemon.getMessageIncoming());
+		assertFalse(daemon.getIncomingMessage());
 
 	}
 
 	/**
-	 * Test method for {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#start()}.
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#getNumberOfOngoingChats(int)}
+	 * .
+	 */
+	public void testGetNumberOfOngoingChats() {
+		BackendEventListener daemon = new BackendEventListener();
+		assertEquals(0, daemon.getNumberOfOngoingChats());
+		TestBuddy tester1 = new TestBuddy();
+		TestBuddy tester2 = new TestBuddy();
+		tester1.setId("1");
+		tester2.setId("2");
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(tester1,
+				"TestMessaggio", "ID_TEST"));
+		assertEquals(1, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(tester1,
+				"TestMessaggio", "ID_TEST"));
+		assertEquals(1, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(tester2,
+				"TestMessaggio", "ID_TEST"));
+		assertEquals(2, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(tester1,
+				"TestMessaggio", "ID_TEST"));
+		assertEquals(2, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST",
+				"Test_MultiChatMessage", "From: Test_Multichat"));
+		assertEquals(3, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST",
+				"Test_MultiChatMessage", "From: Test_Multichat"));
+		assertEquals(3, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST1",
+				"Test_MultiChatMessage", "From: Test_Multichat"));
+		assertEquals(4, daemon.getNumberOfOngoingChats());
+		daemon.onBackendEvent(new MultiChatMessageEvent("ID_TEST",
+				"Test_MultiChatMessage", "From: Test_Multichat"));
+		assertEquals(4, daemon.getNumberOfOngoingChats());
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#emptyHistory(void)}
+	 * .
+	 */
+	public void testEmptyHistory() {
+		BackendEventListener daemon = new BackendEventListener();
+		daemon.onBackendEvent(new ChatMessageReceivedEvent(new TestBuddy(),
+				"TestMessaggio", "ID_TEST"));
+		assertEquals(1, daemon.getNumberOfOngoingChats());
+		daemon.emptyHistory();
+		assertEquals(0, daemon.getNumberOfOngoingChats());
+		// TODO history is emptied when the eC windows gets the focus
+		// this behavior should be more properly tested using mock test
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.uniba.di.cdg.collaborativeworkbench.boot.ui.BackendEventListener#start()}
+	 * .
 	 */
 	@Test
 	public void testStart() {
 		TestNetworkPlugin testNetworkPlugin = new TestNetworkPlugin();
-		
+
 		assertTrue(testNetworkPlugin.getDefault() instanceof TestNetworkPlugin);
 		assertTrue(testNetworkPlugin.getDefault().getRegistry() instanceof TestBackendRegistry);
-		assertTrue(testNetworkPlugin.getDefault().getRegistry().getDescriptors() instanceof Collection<?>);
-		 
+		assertTrue(testNetworkPlugin.getDefault().getRegistry()
+				.getDescriptors() instanceof Collection<?>);
+
 	}
 
 }
