7b0204e9c48fcdbe1815183bed59f9e65e434d94
calefato.uniba@gmail.com <calefato.uniba@gmail.com@d7b81a76-cdef-11de-967b-337e0d75969a>
2012-07-20 14:10:27 +0000

    renamed method

--- trunk/it.uniba.di.cdg.jabber/src/it/uniba/di/cdg/jabber/internal/BuddyGroup.java
+++ trunk/it.uniba.di.cdg.jabber/src/it/uniba/di/cdg/jabber/internal/BuddyGroup.java
@@ -131,7 +131,7 @@ public class BuddyGroup extends AbstractBuddyGroup {
     /* (non-Javadoc)
      * @see it.uniba.di.cdg.xcore.network.model.IEntry#getChilds()
      */
-    public IEntry[] getChilds() {
+    public IEntry[] getChildren() {
 //        final Collection<IBuddy> buddies = ;
         IEntry[] array = new IEntry[buddies.size()];
         return buddies.toArray( array );
